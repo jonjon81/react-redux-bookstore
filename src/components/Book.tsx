a1771cfa8eb9a11e2974255791961716f7e4ef65
Jon Atlas <jjjatlas@hotmail.com>
2023-12-14 00:06:20 +0100

    Update interface

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -3,22 +3,31 @@ import { deleteBook, editBook } from '../redux/actions/BookActions';
 import bookCover from '../../public/book-cover-1.jpg';
 import { FaEdit, FaTrashAlt } from 'react-icons/fa';
 
-const Book = ({ book }: any) => {
+interface Book1 {
+  id: Date;
+  title: string;
+  message: string;
+  price: string;
+  category: string;
+  editing: boolean;
+}
+
+const Book = ({ id, title, message, price, category }: Book1) => {
   const dispatch = useDispatch();
   return (
-    <div className="card mb-4" key={book.id}>
-      <div className="image-component" onClick={() => dispatch(editBook(book.id))}>
+    <div className="card mb-4" key={title}>
+      <div className="image-component" onClick={() => dispatch(editBook(id))}>
         <img className="card-img-top" src={bookCover} alt="Book Cover"></img>
-        <h5 className="card-title">{book.title}</h5>
+        <h5 className="card-title">{title}</h5>
         <div className="icon-container">
           <FaEdit className="fa-edit" />
         </div>
       </div>
       <div className="card-body">
-        <p className="card-price">${book.price}</p>
-        <p className="card-category">{book.category}</p>
-        <p className="card-text book-description">{book.message}</p>
-        <button type="button" className="btn deleteBtn btn-danger" onClick={() => dispatch(deleteBook(book.id))}>
+        <p className="card-price">${price}</p>
+        <p className="card-category">{category}</p>
+        <p className="card-text book-description">{message}</p>
+        <button type="button" className="btn deleteBtn btn-danger" onClick={() => dispatch(deleteBook(id))}>
           <FaTrashAlt />
         </button>
       </div>
